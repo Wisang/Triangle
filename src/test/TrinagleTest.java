a7a2d0413400b045d4fd67d90a4b310553a28881
Wisang <wseom7@gmail.com>
2021-10-27 10:13:44 +0900

    extract constants

--- src/test/TrinagleTest.java
+++ src/test/TrinagleTest.java
@@ -7,11 +7,16 @@ import org.junit.jupiter.api.Test;
 import production.Triangle;
 
 public class TrinagleTest {
+    private static final int INVALID = -1;
+    private static final int GENERAL = 1;
+    private static final int ISOCELES = 2;
+    private static final int EQUILATERAL = 3;
+
     @Test
     void TriangleShapeTest() {
-        assertEquals(3,Triangle.getShape(3,3,3));
-        assertEquals(2,Triangle.getShape(3,3,4));
-        assertEquals(1,Triangle.getShape(3,4,5));
-        assertEquals(-1,Triangle.getShape(3,3,6));
+        assertEquals(EQUILATERAL,Triangle.getShape(3,3,3));
+        assertEquals(ISOCELES,Triangle.getShape(3,3,4));
+        assertEquals(GENERAL,Triangle.getShape(3,4,5));
+        assertEquals(INVALID,Triangle.getShape(3,3,6));
     }
 }
